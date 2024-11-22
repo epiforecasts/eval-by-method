06d1bd9accb5d8140eb4ea640ad72452d57ed079
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2024-11-22 16:36:34 +0000

    move to random effects

--- R/model-wis.R
+++ R/model-wis.R
@@ -30,11 +30,11 @@ m.data <- prep_data(scoring_scale = "log") |>
 m.formula <- log_interval_score ~
   # -----------------------------
   # Method (3 levels*)
-  Method +
+  s(Method, bs = "re") +
   # Number of target countries (2 levels*)
-  CountryTargets +
+  s(CountryTargets, bs = "re") +
   # Trend (3 levels)
-  Trend +
+  s(Trend, bs = "re") +
   # Location (country)
   s(location, bs = "re") +
   # Affiliation same as target country
@@ -52,71 +52,6 @@ m.fit <- bam(formula = m.formula,
              data = m.data,
              family = gaussian())
 
-# Check output
-# summary(m.fit)
-# Parametric terms
-# m.anova <- anova(m.fit)
-
-# Check fit
-# m.aic <- glance(m.fit)$AIC
-# appraise(m.fit)
-
-plot_fixed_coeffs <- function(fit, effects = NULL) {
-  fixed_effects <- attr(m.fit$pterms, "term.labels")
-  fixed <- extract_fixed(m.fit) |>
-    mutate(
-      effect = NA_character_,
-      variable = NA_character_
-    )
-  levels <- c()
-  for (fixed_effect in fixed_effects) {
-    fixed <- fixed |>
-      mutate(
-        effect = if_else(
-          grepl(paste0("^", fixed_effect), term),
-          fixed_effect,
-          effect
-        ),
-        variable = if_else(
-          effect == fixed_effect,
-          sub(paste0("^", fixed_effect), "", term),
-          variable
-        )
-      ) |>
-      complete(
-        variable = levels(m.data[[fixed_effect]]),
-        fill = list(
-          value = 0, lower_2.5 = 0, upper_97.5 = 0, effect = fixed_effect
-        )
-      )
-    levels <- c(levels, levels(m.data[[fixed_effect]]))
-  }
-  fixed <- fixed |>
-    filter(!is.na(variable)) |>
-    select(effect, variable, value, lower_2.5, upper_97.5)
-  max_effect <- max(abs(fixed$lower_2.5), abs(fixed$upper_97.5))
-
-  fixed |>
-    mutate(
-      effect = factor(effect, levels = unique(effect)),
-      variable = factor(variable, levels = rev(levels))
-    )
-  if (!is.null(effects)) {
-    fixed <- fixed |>
-      filter(effect %in% effects)
-  }
-  fixed <- fixed |>
-    ggplot(aes(x = variable, y = value, colour = effect)) +
-    geom_point() +
-    geom_linerange(aes(ymin = lower_2.5, ymax = upper_97.5)) +
-    geom_hline(yintercept = 0, lty = 2) +
-    labs(y = "Partial effect", x = NULL, col = NULL) +
-    scale_colour_brewer(type = "qual", palette = 2) +
-    expand_limits(y = c(-max_effect, max_effect)) +
-    coord_flip() +
-    theme(legend.position = "bottom")
-}
-
 plot_models <- function(fit) {
   extract_ranef(m.fit) |>
     filter(group_var == "Model") |>
@@ -124,7 +59,7 @@ plot_models <- function(fit) {
     left_join(targets |> rename(group = model)) |>
     mutate(group = sub(".*-", "", group)) |> ## remove institution identifier
     select(-group_var) |>
-    arrange(desc(value)) |>
+    arrange(value) |>
     mutate(group = factor(group, levels = as.character(group))) |>
     ggplot(aes(x = group, col = classification, shape = target_type)) +
     geom_point(aes(y = value)) +
@@ -132,6 +67,31 @@ plot_models <- function(fit) {
     geom_hline(yintercept = 0, lty = 2) +
     labs(y = "Partial effect", x = NULL, colour = NULL, shape = NULL) +
     scale_colour_brewer(type = "qual", palette = 2) +
-    coord_flip() +
-    theme(legend.position = "bottom")
+    theme(
+      legend.position = "bottom",
+      axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1)
+    )
 }
+
+plot_effects <- function(fit) {
+  extract_ranef(m.fit) |>
+    filter(!(group_var %in% c("Model", "location"))) |>
+    mutate(group = factor(group, levels = as.character(rev(group)))) |>
+    ggplot(aes(x = group, col = group_var)) +
+    geom_point(aes(y = value)) +
+    geom_linerange(aes(ymin = lower_2.5, ymax = upper_97.5)) +
+    geom_hline(yintercept = 0, lty = 2, alpha = 0.25) +
+    labs(y = "Partial effect", x = NULL, colour = NULL, shape = NULL) +
+    scale_colour_brewer(type = "qual", palette = "Dark2") +
+    theme(
+      legend.position = "bottom",
+      axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1)
+    ) +
+    coord_flip()
+}
+
+p <- plot_models(m.fit)
+ggsave("model_performance.png", p, width = 7.5, height = 3.5)
+
+effects <- plot_effects(m.fit)
+ggsave("model_effects.png", effects, width = 6, height = 3.5)
