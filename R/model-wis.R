80bd09bf344f22f66e5926ea4b3a5bffc7b310e2
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2025-02-11 14:59:55 +0000

    add cases to analysis (#20)
    * add cases to analysis
    
    * get observed
    
    * remove ref
    
    * fix typo

--- R/model-wis.R
+++ R/model-wis.R
@@ -14,14 +14,10 @@ theme_set(theme_classic())
 source(here("R", "prep-data.R"))
 
 # --- Get data ---
-data <- map(c("case", "death"), \(target) {
-  prep_data(scoring_scale = "log", data_type = target) |>
-    mutate(target = target)
-}) |>
-  bind_rows() |>
-  mutate(target = factor(target))
+data <- prep_data(scoring_scale = "log")
+outcomes <- unique(data$outcome_target)
 classification <- classify_models()
-targets <- read_csv(here("data", "targets-by-model.csv"))
+targets <- table_targets(data)
 
 m.data <- data |>
   filter(!grepl("EuroCOVIDhub-", Model)) |>
@@ -54,43 +50,78 @@ m.formula <- log_wis ~
   s(Model, bs = "re")
 
 # Fit GAMM with normal distribution
-m.fit <- bam(formula = m.formula,
-             data = m.data |> filter(target == "death"),
-             family = gaussian())
+m.fits <- outcomes |>
+  set_names() |>
+  map(\(outcome) {
+  bam(
+    formula = m.formula,
+    data = m.data |> filter(outcome_target == outcome),
+    family = gaussian()
+  )
+})
 
-plot_models <- function(fit) {
-  extract_ranef(m.fit) |>
-    filter(group_var == "Model") |>
-    left_join(classification |> rename(group = model)) |>
-    left_join(targets |> rename(group = model)) |>
-    mutate(group = sub(".*-", "", group)) |> ## remove institution identifier
-    select(-group_var) |>
-    arrange(value) |>
-    mutate(group = factor(group, levels = as.character(group))) |>
-    ggplot(aes(x = group, col = classification, shape = target_type)) +
-    geom_point(aes(y = value)) +
-    geom_linerange(aes(ymin = lower_2.5, ymax = upper_97.5)) +
-    geom_hline(yintercept = 0, lty = 2) +
-    labs(y = "Partial effect", x = NULL, colour = NULL, shape = NULL) +
-    scale_colour_brewer(type = "qual", palette = 2) +
-    theme(
-      legend.position = "bottom",
-      axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1)
-    )
+plot_models <- function(fits, scores, x_labels = TRUE) {
+  outcomes <- unique(scores$outcome_target)
+  classification <- classify_models() |>
+    rename(group = model)
+  targets <- table_targets(scores) |>
+    select(group = Model, CountryTargets) |>
+    distinct()
+  plots <- map(fits, function(fit) {
+    plot <- extract_ranef(fit) |>
+      filter(group_var == "Model") |>
+      left_join(classification) |>
+      left_join(targets) |>
+      mutate(group = sub(".*-", "", group)) |> ## remove institution identifier
+      select(-group_var) |>
+      arrange(value) |>
+      mutate(group = factor(group, levels = unique(as.character(group)))) |>
+      ggplot(aes(x = group, col = classification, shape = CountryTargets)) +
+      geom_point(aes(y = value)) +
+      geom_linerange(aes(ymin = lower_2.5, ymax = upper_97.5)) +
+      geom_hline(yintercept = 0, lty = 2) +
+      labs(y = "Partial effect", x = "Model", colour = NULL, shape = NULL) +
+      scale_colour_brewer(type = "qual", palette = 2) +
+      theme(
+        legend.position = "bottom",
+        axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1)
+      )
+    if (!x_labels) {
+      plot <- plot +
+        theme(
+          axis.text.x = element_blank(),
+          axis.ticks.x = element_blank()
+        )
+    }
+    return(plot)
+  })
+  ## remove legends
+  if (length(plots) > 1) {
+    for (i in seq_len(length(plots) - 1)) {
+      plots[[i]] <- plots[[i]] + theme(legend.position = "none")
+    }
+  }
+  for (i in seq_along(plots)) {
+    plots[[i]] <- plots[[i]] + ggtitle(outcomes[i])
+  }
+  Reduce(`+`, plots) + plot_layout(ncol = 1)
 }
 
-plot_effects <- function(fit) {
-  extract_ranef(m.fit) |>
+plot_effects <- function(fits, scores) {
+  map(fits, extract_ranef) |>
+    bind_rows(.id = "outcome_target") |>
     filter(!(group_var %in% c("Model", "location"))) |>
-    mutate(group = factor(group, levels = as.character(rev(group)))) |>
+    mutate(group = factor(group, levels = unique(as.character(rev(group))))) |>
     ggplot(aes(x = group, col = group_var)) +
     geom_point(aes(y = value)) +
     geom_linerange(aes(ymin = lower_2.5, ymax = upper_97.5)) +
     geom_hline(yintercept = 0, lty = 2, alpha = 0.25) +
+    facet_wrap(~outcome_target, scales = "free_y") +
     labs(y = "Partial effect", x = NULL, colour = NULL, shape = NULL) +
-    scale_colour_brewer(type = "qual", palette = "Dark2") +
+    scale_colour_brewer(type = "qual", palette = "Set1") +
     theme(
       legend.position = "bottom",
+      strip.background = element_blank(),
       axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1)
     ) +
     coord_flip()
--- R/prep-data.R
+++ R/prep-data.R
@@ -25,23 +25,26 @@ classify_models <- function(file = here("data", "model-classification.csv")) {
 }
 
 # Prepare scores data with explanatory variables
-prep_data <- function(scoring_scale = "log", data_type = "death") {
+prep_data <- function(scoring_scale = "log") {
+  scores_files <- list.files(here("data"), pattern = "scores-raw-.*\\.csv")
+  names(scores_files) <- sub("scores-raw-(.*)\\..*$", "\\1", scores_files)
   # Get raw interval score
-  scores_raw <- read_csv(
-    here("data", paste0("scores-raw-", data_type, ".csv"))
-    ) |>
+  scores_raw <- scores_files |>
+    map(\(file) {
+      read_csv(here("data", file))
+    }) |>
+    bind_rows(.id = "outcome_target") |>
     filter(scale == scoring_scale)
 
   # Extra explanatory vars ------
   # Target type
-  targets <- scores_raw |>
+  country_targets <- scores_raw |>
     select(model, forecast_date, location) |>
     distinct() |>
     group_by(model, forecast_date) |>
-    summarise(target_count = n()) |>
-    ungroup() |>
+    summarise(target_count = n(), .groups = "drop") |>
     group_by(model) |>
-    summarise(CountryTargets = all(target_count == 1)) |>
+    summarise(CountryTargets = all(target_count == 1), .groups = "drop") |>
     mutate(CountryTargets = factor(CountryTargets,
                                 levels = c(TRUE, FALSE),
                                 labels = c("Single-country",
@@ -52,17 +55,21 @@ prep_data <- function(scoring_scale = "log", data_type = "death") {
     select(model, Method = classification)
 
   # Incidence level + trend (see: R/import-data.r)
-  obs <- read_csv(here("data", "observed.csv")) |>
+  obs <- names(scores_files) |>
+    set_names() |>
+    map(get_observed) |>
+    bind_rows(.id = "outcome_target") |>
     mutate(Trend = factor(trend, levels = c("Stable", "Increasing", "Decreasing"))) |>
     rename(Incidence = observed) |>
-    select(target_end_date, location, Trend, Incidence)
+    select(target_end_date, location, outcome_target, Trend, Incidence)
 
   data <- scores_raw |>
-    left_join(obs, by = c("location", "target_end_date")) |>
-    left_join(targets, by = "model") |>
+    left_join(obs, by = c("location", "target_end_date", "outcome_target")) |>
+    left_join(country_targets, by = "model") |>
     left_join(methods, by = "model") |>
     rename(Model = model, Horizon = horizon) |>
     mutate(Model = as.factor(Model),
+           outcome_target = paste0(str_to_title(outcome_target), "s"),
            Horizon = ordered(Horizon,
                              levels = 1:4, labels = 1:4),
            log_wis = log(wis + 0.01))
