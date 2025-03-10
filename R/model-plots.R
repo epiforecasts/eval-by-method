2f5cafda81c79f00e9c88913f35dc7b35f66f79b
Kath Sherratt <62290797+kathsherratt@users.noreply.github.com>
2025-03-10 03:22:50 -0700

    Add univariate models (#68)
    * Update model-wis.R
    
    - Add univariate models for model structure and country targets
    - Get effects from all models and output as tibble
    - Tidy up code comments
    
    * Update model-wis.R
    
    * Update model-plots.R
    
    - Plot adjusted and unadjusted estimates  together
    - Remove showing effects for epidemic trend, as we don't show effects for any of the other (confounding) variables
    
    * Update random-effects.rds
    
    Save as single tibble with unadjusted/adjusted effects
    
    * Update results code
    
    Text checked for accuracy but basically unchanged
    
    * fix fig3 model plot to work with combined random effects df
    
    the models are now not neatly ordered so needs another fix for that.
    
    * Add univariate fit for each model
    
    belts and braces, to display differences for each model on fig 3
    
    * Update model-plots.R
    
    show un/adjusted in fig3 same as fig 2
    
    * update report
    
    ---------
    
    Co-authored-by: Sebastian Funk <sebastian.funk@lshtm.ac.uk>

--- R/model-plots.R
+++ R/model-plots.R
@@ -6,33 +6,50 @@ library("gammit")
 source(here("R", "prep-data.R"))
 source(here("R", "descriptive.R"))
 
-plot_models <- function(random_effects, scores, x_labels = TRUE) {
-  outcomes <- unique(scores$outcome_target)
+plot_models <- function(random_effects, scores, x_labels = TRUE,
+                        anonymise = TRUE) {
   classification <- classify_models() |>
     rename(group = model)
   targets <- table_targets(scores) |>
     select(group = Model, CountryTargets) |>
     distinct()
-  plots <- map(random_effects, function(effects) {
-    plot <- effects |>
-      filter(group_var == "Model") |>
-      left_join(classification) |>
-      left_join(targets) |>
-      group_by(classification, CountryTargets) |>
-      mutate(group = paste(classification, CountryTargets, row_number())) |>
-      select(-group_var) |>
-      arrange(-value) |>
-      ungroup() |>
-      mutate(group = factor(group)) |>
-      ggplot(aes(x = group, col = classification, shape = CountryTargets)) +
-      geom_point(aes(y = value)) +
-      geom_linerange(aes(ymin = lower_2.5, ymax = upper_97.5)) +
+  effects <- random_effects |>
+    filter(group_var == "Model") |>
+    left_join(classification) |>
+    left_join(targets)
+  models <- effects |>
+    select(classification, CountryTargets, group) |>
+    distinct() |>
+    group_by(classification, CountryTargets) |>
+    mutate(
+      id = row_number(),
+      anon_group = paste(classification, CountryTargets, id),
+      ) |>
+    ungroup() |>
+    arrange(classification, CountryTargets, id) |>
+    mutate(anon_group = factor(anon_group, levels = rev(unique(anon_group)))) |>
+    select(group, anon_group)
+  group_var <- ifelse(anonymise, "anon_group", "group")
+  plot <- effects |>
+      left_join(models, by = "group") |>
+      mutate(
+        Model = factor(model, levels = c("Adjusted", "Unadjusted"))
+      ) |>
+      ggplot(aes(x = .data[[group_var]], col = classification,
+                 shape = CountryTargets, lty = Model, alpha = Model)) +
+      geom_point(aes(y = value),
+                 position = position_dodge(width=1)) +
+      geom_linerange(aes(ymin = lower_2.5, ymax = upper_97.5),
+                     position = position_dodge(width=1)) +
       geom_hline(yintercept = 0, lty = 2) +
       labs(y = "Partial effect", x = "", colour = NULL, shape = NULL) +
+      scale_alpha_manual(values = c("Adjusted" = 1, "Unadjusted" = 0.3)) +
       scale_colour_brewer(type = "qual", palette = 2) +
+      facet_wrap(~outcome_target, scales = "free_y", drop = TRUE) +
       theme(
         legend.position = "bottom",
-        axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1)
+        axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1),
+        strip.background = element_blank()
       ) +
       coord_flip()
     if (!x_labels) {
@@ -42,30 +59,28 @@ plot_models <- function(random_effects, scores, x_labels = TRUE) {
           axis.ticks.y = element_blank()
         )
     }
-    return(plot)
-  })
-  ## remove legends
-  plots <- map(seq_along(plots), \(i) {
-    plots[[i]] + ggtitle(outcomes[i])
-  })
-  Reduce(`+`, plots) + plot_layout(ncol = 2, guides = "collect") &
-    theme(legend.position = "bottom")
+  return(plot)
 }
 
 plot_effects <- function(random_effects) {
   random_effects |>
-    bind_rows(.id = "outcome_target") |>
-    filter(!(group_var %in% c("Model", "location", "Trend"))) |>
-    mutate(group = factor(group, levels = unique(as.character(rev(group))))) |>
-    ggplot(aes(x = group, col = group_var)) +
-    geom_point(aes(y = value)) +
-    geom_linerange(aes(ymin = lower_2.5, ymax = upper_97.5)) +
+    filter(group_var %in% c("Method", "CountryTargets")) |>
+    mutate(group = factor(group, levels = unique(as.character(rev(group)))),
+           Model = factor(model, levels = c("Adjusted", "Unadjusted"))) |>
+    ggplot(aes(x = group, col = group_var,
+               lty = Model, alpha = Model)) +
+    geom_point(aes(y = value),
+               position = position_dodge(width=1)) +
+    geom_linerange(aes(ymin = lower_2.5, ymax = upper_97.5,),
+                   position = position_dodge(width=1)) +
     geom_hline(yintercept = 0, lty = 2, alpha = 0.25) +
+    scale_alpha_manual(values = c("Adjusted" = 1, "Unadjusted" = 0.3)) +
     facet_wrap(~outcome_target, scales = "free_y") +
     labs(y = "Partial effect", x = NULL, colour = NULL, shape = NULL) +
-    scale_colour_brewer(type = "qual", palette = "Set1") +
+    scale_colour_brewer(type = "qual", palette = "Set1",
+                        guide = FALSE) +
     theme(
-      legend.position = "none",
+      legend.position = "bottom",
       strip.background = element_blank(),
       axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1)
     ) +
--- R/model-wis.R
+++ R/model-wis.R
@@ -26,30 +26,34 @@ m.data <- data |>
   ) |>
   ungroup()
 
-# --- Model ---
-# Formula
+# --- Model formula ---
+# Univariate for explanatory variables
+m.formula_uni_type <- wis ~ s(Method, bs = "re")
+m.formula_uni_tgt <- wis ~ s(CountryTargets, bs = "re")
+m.formula_uni_model <- wis ~ s(Model, bs = "re")
+
+# Full model
 m.formula <- wis ~
-  # -----------------------------
-  # Method (3 levels*)
+  # Method
   s(Method, bs = "re") +
-  # Number of target countries (2 levels*)
+  # Number of target countries
   s(CountryTargets, bs = "re") +
-  # Trend (3 levels)
-  s(Trend, bs = "re") +
-  # Location (country)
-  s(location, bs = "re") +
-  # Affiliation same as target country
-  # CountryTargetAffiliated +
   # -----------------------------
-  # Observed incidence: interacting with trend; thin plate reg. spline (default)
+  # Trend
+  s(Trend, bs = "re") +
+  # Location
+  s(location, bs = "re") +
+  # Week
   s(time, by = location, k = 40) +
-  # Horizon (4 levels, ordinal)
+  # Horizon
   s(Horizon, k = 3, by = Model, bs = "sz") +
-  # Individual model (35 levels*): random effect, nested within method
+  # Individual model
   s(Model, bs = "re")
 
-# Fit GAMM with normal distribution
-m.fits <- outcomes |>
+# --- Model fitting ---
+# Set up to fit to each outcome target (cases, deaths)
+m.fit <- function(outcomes, m.formula) {
+  outcomes |>
   set_names() |>
   map(\(outcome) {
     bam(
@@ -59,9 +63,27 @@ m.fits <- outcomes |>
       control = gam.control(trace = TRUE)
     )
   })
+}
+# Fit
+m.fits_uni_type <- m.fit(outcomes, m.formula_uni_type)
+m.fits_uni_tgt <- m.fit(outcomes, m.formula_uni_tgt)
+m.fits_uni_model <- m.fit(outcomes, m.formula_uni_model)
+m.fits_full <- m.fit(outcomes, m.formula)
 
-random_effects <- map(m.fits, extract_ranef)
-checks <- map(m.fits, k.check)
+# --- Output handling ---
+# Extract estimates for random effects
+random_effects_uni <- map_df(
+  c(m.fits_uni_type, m.fits_uni_tgt, m.fits_uni_model),
+  extract_ranef,
+  .id = "outcome_target") |>
+  mutate(model = "Unadjusted")
+
+random_effects <- map_df(m.fits_full, extract_ranef,
+                        .id = "outcome_target") |>
+  mutate(model = "Adjusted") |>
+  bind_rows(random_effects_uni)
+
+checks <- map(m.fits_full, k.check)
 
 saveRDS(random_effects, here("output", "random-effects.rds"))
 saveRDS(checks, here("output", "checks.rds"))
