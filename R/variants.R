e48d02735d878dfa3946ffd3287c4854407ad684
kathsherratt <kathsherratt@gmail.com>
2025-12-06 20:39:17 +0000

    Update variants.R

--- R/variants.R
+++ R/variants.R
@@ -1,112 +1,101 @@
-# Gets variants of concern dataset from ECDC
-# For each variant/country finds the first period
-#  with variant % greater than 5% and up to 50%
-#  before the first peak for that variant
-# Note ECDC data are in weeks, dates are indexed to Saturdays to match target end dates
-#
-# Example
-#   variant_names <- c("B.1.617.2" = "Delta", "B.1.1.529" = "Omicron")
-#   forecast_variants <- download_variant_introduction(introduction_percent = 5,
-#                                                   country_names = "Germany",
-#                                                   variant_codes = names(variant_names))
+# Identifying dominant variant phases across 30 European countries
+
 library(here)
 library(dplyr)
+library(tidyr)
 library(readr)
 library(lubridate)
+library(ggplot2)
+library(plotly)
 
+# Process variants data
+variants_raw <- read_csv("https://opendata.ecdc.europa.eu/covid19/virusvariant/csv/data.csv",
+                         progress = FALSE, show_col_types = FALSE)
 variant_names <- c("Other" = "Other",
                    "Unknown" = "UNK",
+                   "Other" = "SGTF",
                    "Alpha" = "B.1.1.7",
+                   "Alpha" = "B.1.1.7+E484K",
+                   "Other" = "B.1.351", # Beta
+                   "Other" = "P.1", # Gamma
+                   "Other" = "P.3", # Gamma
+                   "Delta" = "B.1.617.1",
                    "Delta" = "B.1.617.2",
-                   "Omicron" = "B.1.1.529",
-                   "Omicron-BA1" = "BA.1")
+                   "Delta" = "B.1.617.3",
+                   "Delta" = "AY.4.2",
+                   "Other" = "B.1.427", # Epsilon
+                   "Other" = "B.1.429", # Epsilon
+                   "Other" = "B.1.427/B.1.429",
+                   "Other" = "B.1.526", # Kappa
+                   "Other" = "B.1.525", # Eta
+                   "Other" = "C.37", # Lambda
+                   "Other" = "B.1.620",
+                   "Other" = "B.1.621", # Mu
+                   "Omicron/BA.1" = "B.1.1.529", # Omicron
+                   "Omicron/BA.1" = "BA.1",
+                   "Omicron-BA.2" = "BA.2",
+                   "Omicron-BA.2" = "BA.2+L452X",
+                   "Omicron-BA.2" = "BA.2.75",
+                   "Omicron-BA.2" = "BA.2.86",
+                   "Other" = "BA.3",
+                   "Omicron-BA.4/5" = "BA.4/BA.5",
+                   "Omicron-BA.4/5" = "BA.4",
+                   "Omicron-BA.4/5" = "BA.5",
+                   "Omicron-BQ/XBB" = "BQ.1",
+                   "Omicron-BQ/XBB" = "XBB",
+                   "Omicron-BQ/XBB" = "XBB.1.5-like",
+                   "Omicron-BQ/XBB" = "XBB.1.5-like+F456L")
 
-  # date lookup table for ecdc year-weeks
-  date_range <- tibble(
-    daily_date = seq.Date(from = as.Date("2020-01-04"), to = Sys.Date(), by = 7),
-    year = isoyear(daily_date),
-    week = isoweek(daily_date)) |>
-    group_by(year, week) |>
-    filter(daily_date == min(daily_date)) |>
-    mutate(forecast_period = between(daily_date,
-                                     as.Date("2021-03-01"),
-                                     as.Date("2023-03-17")))
+# complete grid of dates and locations in ecdc year-weeks
+date_location <- tibble(
+  daily_date = seq.Date(from = as.Date("2020-01-04"), to = Sys.Date(), by = 7),
+  year = isoyear(daily_date),
+  week = isoweek(daily_date)) |>
+  group_by(year, week) |>
+  filter(daily_date == min(daily_date)) |>
+  filter(between(daily_date,
+                 as.Date("2021-03-01"), as.Date("2023-03-17"))) |>
+  expand_grid(country = unique(variants_raw$country))
 
-  # get data
-  variants_raw <- read_csv("https://opendata.ecdc.europa.eu/covid19/virusvariant/csv/data.csv",
-                       progress = FALSE, show_col_types = FALSE)
+# filter to relevant dates
+variants <- variants_raw |>
+  # set dates as a complete grid of location-weeks
+  mutate(year = as.numeric(substr(year_week, 1, 4)),
+         week = as.numeric(substr(year_week, 6, 8))) |>
+  left_join(date_location, by = c("year", "week", "country")) |>
+  filter(!is.na(daily_date)) |>
+  select(country, year_week, daily_date, source,
+         number_sequenced, variant, percent_variant)
 
-  # filter as needed and set dates
-  variants <- variants_raw |>
-    filter(source == "GISAID") |>
-    mutate(year = as.numeric(substr(year_week, 1, 4)),
-           week = as.numeric(substr(year_week, 6, 8))) |>
-    left_join(date_range, by = c("year", "week")) |>
-    filter(forecast_period) |>
-    select(country, daily_date, variant, percent_variant) |>
-    mutate(dominant = percent_variant >= 50,
-           variant_name = factor(variant,
-                                 levels = variant_names,
-                                 labels = names(variant_names)))
-
-  variants |>
-    ggplot(aes(x = daily_date, y = percent_variant, col = variant_name)) +
-    geom_line() +
-    facet_wrap(~ country, ncol = 1) +
-    theme(legend.position = "bottom")
-  ggsave("variants.pdf", height = 20)
-
-
-
-  # get the first maxima of the variant
-  introduction_period <- variants |>
-    group_by(variant, country) |>
-    arrange(daily_date) |>
-    filter(percent_variant <= 99) |>
-    slice_max(percent_variant, with_ties = FALSE) |>
-    select(variant, country, variant_peak_date = daily_date)
-
-  # get the period before the first maxima where the variant is e.g. >5<50%
-  var_data <- variants |>
-    left_join(introduction_period, by = c("variant", "country")) |>
-    filter(daily_date <= variant_peak_date &
-             percent_variant >= introduction_percent &
-             percent_variant <= dominant_percent) |>
-    group_by(country, variant) |>
-    summarise(date_introduction = min(daily_date),
-              date_dominant = max(daily_date) + 7,
-              date_peak = min(variant_peak_date))
+# group variants into named categories
+variants <- variants |>
+  mutate(variant_name = forcats::fct_recode(variant, !!!variant_names)) |>
+  # remove "unknown" as counted in Other
+  filter(!variant %in% c("UNK")) |>
+  # sum variants by week
+  group_by(country, year_week, daily_date,
+           source, number_sequenced, variant_name) |>
+  summarise(percent_variant = sum(percent_variant),
+            .groups = "drop")
 
 
-# Score by variant phase -----------------------------------------
+# Identify dominant variant phases -------------------------------------------
+dominant_2 <- variants |>
+  group_by(country, year_week, daily_date, source, number_sequenced) |>
+  slice_max(order_by = percent_variant, with_ties = FALSE, n = 2) |>
+  mutate(variant_name = as.character(variant_name),
+         rank = row_number()) |>
+  ungroup() |>
+  pivot_wider(names_from = source,
+              values_from = c(number_sequenced,
+                              variant_name, percent_variant),
+              names_glue = "{source}_{.value}",
+              values_fill = NA, id_cols = c(country, year_week, daily_date, rank))
 
-
-delta <- variant_data |>
-  filter(variant == "B.1.617.2") |>
-  select(location_name = country,
-         delta_intro = date_introduction,
-         delta_dominant = date_dominant)
-
-omicron <- variant_data |>
-  filter(variant == "BA.1") |>
-  select(location_name = country,
-         omicron_intro = date_introduction,
-         omicron_dominant = date_dominant)
-
-locations <- read_csv("https://raw.githubusercontent.com/covid19-forecast-hub-europe/covid19-forecast-hub-europe/main/data-locations/locations_eu.csv") |>
-  select(location_name, location)
-
-scores_raw_var <- scores_raw |>
-  filter(scale == "log") |>
-  # distinct(location, target_end_date) |>
-  left_join(locations, by = "location") |>
-  left_join(delta, by = "location_name") |>
-  left_join(omicron, by = "location_name") |>
-  mutate(variant = case_when(
-    target_end_date <= delta_intro ~ "pre-delta",
-    target_end_date >= delta_intro & target_end_date <= delta_dominant ~ "delta intro",
-    target_end_date >= delta_dominant & target_end_date <= omicron_intro ~ "delta dominant",
-    target_end_date >= omicron_intro & target_end_date <= omicron_dominant ~ "omicron intro",
-    target_end_date >= omicron_dominant ~ "omicron dominant"
-  )) |>
-  filter(!is.na(variant))  # exclude countries with no variant data (UK, Swiss)
+# Apply rules to identify dominant variant phase, using the top 2 variants each week
+# (1) Prefer GISAID for known reliability: For the first choice, prefer the GISAID first-ranked variant.
+# (2) Avoiding "Other" category: If GISAID is "Other" and TESSy has a named variant, take TESSy. If the second-ranked variant is the same as the week before, and the first-ranked variant is "Other", take the second-ranked variant.
+# (3) Temporal consistency: If the first-ranked variant is different to the week after and the week before, and the second-ranked variant appears in any source in both the week after and the week before, take the second-ranked variant from that source.
+# TODO
+# Apply these rules in order.
+# Add new columns with: dominant variant name, dominant variant percentage, number sequenced, source, rule used to identify dominant variant.
