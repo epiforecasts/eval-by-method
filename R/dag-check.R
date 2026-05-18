28651ffc584726a6623318d8c8d048dd777a25fc
kathsherratt <kathsherratt@gmail.com>
2026-05-18 20:52:48 +0100

    Update dag-check.R

--- R/dag-check.R
+++ R/dag-check.R
@@ -1,51 +1,45 @@
-# Define DAG for model structure and WIS
-# Variables
-# Method: model structure (exposure)
-# wis: weighted interval score (outcome)
-# team_resources: resources available to the team (latent, unobserved)
-# CountryTargets: single- vs multi-country model
-# epidemic_state: latent epidemic transmission dynamics at (Location, date)
-# Location: country target location (cause of epidemic_state)
-# VariantPhase: dominant variant phase at forecast target date (cause of epidemic_state)
-# Trend: increasing, decreasing, or stable epidemic trend (observable proxy of epidemic_state)
-# Incidence: observed incidence level at forecast target date (observable proxy of epidemic_state)
-# Horizon: forecast horizon
-
+# Define DAG: methods -> score
 library(dagitty)
 library(ggdag)
 library(ggplot2)
 
-dag <- dagitty('dag {
-  Method[exposure]
-  wis[outcome]
-  team_resources[latent]
-  epidemic_state[latent]
+dag <- dagitty('
+dag {
+bb="-6.421,-7.624,6.826,6.874"
+"Accuracy score" [outcome,pos="5.506,2.291"]
+"Epidemic dynamics" [latent,pos="1.743,-0.912"]
+"Model structure" [exposure,pos="-2.105,2.885"]
+"Modeller strategy" [latent,pos="-2.133,-1.027"]
+"Observed incidence" [adjusted,pos="3.234,0.948"]
+"Single/multi-country" [pos="0.210,1.926"]
+Country [adjusted,pos="-0.231,-0.950"]
+Horizon [adjusted,pos="2.155,1.907"]
+Model [adjusted,pos="1.658,3.000"]
+Prediction [pos="3.418,3.058"]
+Trend [adjusted,pos="3.262,-0.164"]
+Variant [adjusted,pos="0.210,-1.794"]
+"Epidemic dynamics" -> "Observed incidence"
+"Epidemic dynamics" -> Trend
+"Model structure" -> "Single/multi-country"
+"Model structure" -> Model
+"Modeller strategy" -> "Model structure"
+"Modeller strategy" -> "Single/multi-country"
+"Observed incidence" -> "Accuracy score"
+"Observed incidence" <-> Trend
+"Single/multi-country" -> Prediction
+Country -> "Epidemic dynamics"
+Country -> "Observed incidence"
+Horizon -> Prediction
+Model -> Prediction
+Prediction -> "Accuracy score"
+Variant -> "Epidemic dynamics"
+}
+')
 
-  team_resources -> Method
-  team_resources -> CountryTargets
-  team_resources -> wis
+tidy_ggdag <- tidy_dagitty(dag)
 
-  CountryTargets -> wis
-  Method -> wis
-
-  Location -> epidemic_state
-  VariantPhase -> epidemic_state
-  epidemic_state -> Trend
-  epidemic_state -> Incidence
-  epidemic_state -> wis
-
-  Horizon -> wis
-}')
-
-# adjustmentSets(dag, effect = "total")
-
-# adjustmentSets(dag, effect = "direct")
-
-# impliedConditionalIndependencies(dag)
-
-# paths(dag, from = "Method", to = "wis")
+p_adj <- ggdag_adjustment_set(tidy_ggdag, node_size = 14) +
+  theme(legend.position = "bottom")
 
 p <- ggdag_status(dag, text = FALSE, use_labels = "name") +
-  theme_dag()
-
-ggsave("report/_quarto/supplement/dag.pdf", p, width = 8, height = 6)
+  theme_dag_blank()
